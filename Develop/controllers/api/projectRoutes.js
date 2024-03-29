2aa7e9bd3e185703fec2e0ae7b7bf76a331a3cd7
Joseph Kamil <kamilj@umich.edu>
2024-03-29 00:07:49 -0700

    login page works

--- Develop/controllers/api/projectRoutes.js
+++ Develop/controllers/api/projectRoutes.js
@@ -8,31 +8,14 @@ router.get('/', async (req, res) => {
       // order: [['name', 'ASC']],
     });
 
-    const users = userData.map(project => project.get({ plain: true }));
+    const users = userData.map((project) => project.get({ plain: true }));
 
     if (!userData) {
       res.status(404).json(userData);
     }
     res.render('homepage', {
       users,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/:id', async (req, res) => {
-  try {
-    const category = await Project.findByPk(req.params.id, {});
-    if (!category) {
-      return res.status(404).json({ msg: 'Project not found' });
-    }
-
-    const projects = category.get({ plain: true });
-
-    res.render('project', {
-      projects
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -48,11 +31,28 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const category = await Project.findByPk(req.params.id, {});
+    if (!category) {
+      return res.status(404).json({ msg: 'Project not found' });
+    }
+
+    const projects = category.get({ plain: true });
+
+    res.render('project', {
+      projects,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.post('/', async (req, res) => {
   try {
     const newProject = await Project.create({
       ...req.body,
-      user_id: req.session.user_id
+      user_id: req.session.user_id,
     });
 
     res.status(200).json(newProject);
@@ -66,8 +66,8 @@ router.delete('/:id', async (req, res) => {
     const projectData = await Project.destroy({
       where: {
         id: req.params.id,
-        user_id: req.session.user_id
-      }
+        user_id: req.session.user_id,
+      },
     });
 
     if (!projectData) {
